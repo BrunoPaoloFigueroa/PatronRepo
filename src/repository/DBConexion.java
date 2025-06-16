0148ee30649961dbb9731f5914fb53916f332743
Figueroa_BP <bruno.figueroa@usil.pe>
2025-06-16 18:06:59 -0500

    Update DBConexion.java

--- src/repository/DBConexion.java
+++ src/repository/DBConexion.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConexion {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/banco";
     private static final String USER = "root";
-    private static final String PASSWORD = "T3csup5629";
+    private static final String PASSWORD = "";
 
     public static Connection getConexion() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
